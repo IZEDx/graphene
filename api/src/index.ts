ad61faf084588c64110ef6127b1effdb857bcbbd
Niklas Kühtmann <nk@ized.io>
2020-08-22 08:33:21 +0200

    ✨ Add exports

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,2 +1,13 @@
 
 export * from "./server";
+export * from "./models/enums/UserRole";
+export * from "./models/scalars/RichContent";
+export * from "./models/scalars/Password";
+export * from "./models/KeyValue";
+export * from "./models/User";
+export * from "./services/UserService";
+export * from "./validators/SafePassword";
+export * as ORM from "typeorm";
+export * as API from "type-graphql";
+export * as DI from "typedi";
+export * as CV from "class-validator";
